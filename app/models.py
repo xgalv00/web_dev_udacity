2ec759a6b329eeff6509a038b3610eb0d1a30fe9
xgalv00 <galkin.vitaly@gmail.com>
2013-11-16 12:45:37 +0200

    lesson_5: One post json representation works

--- app/models.py
+++ app/models.py
@@ -4,8 +4,8 @@ from google.appengine.ext import db
 
 
 class Blog(db.Model):
-    title = db.StringProperty(required=True)
-    blog_entry = db.TextProperty(required=True)
+    subject = db.StringProperty(required=True)
+    content = db.TextProperty(required=True)
     created = db.DateTimeProperty(auto_now_add=True)
 
 
